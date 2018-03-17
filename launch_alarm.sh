e30666fe0dd2489fc46bef2c4a63fd66da9ffe36
rodskin <rod.gilles@gmail.com>
2018-03-17 19:09:24 +0100

    add volume loop

--- launch_alarm.sh
+++ launch_alarm.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+volume=1
+sleep_volume_delay=5
 mpc clear
 mpc random 1
-mpc volume 1
+mpc volume ${volume}
 mpc add spotify:user:icelandairwaves:playlist:3dNCFy3Q9d6LtGZLWT0c2O
 mpc play
+
+sleep ${sleep_volume_delay}
+for volume in {1..40}
+do
+   mpc volume ${volume}
+   sleep ${sleep_volume_delay}
+done
