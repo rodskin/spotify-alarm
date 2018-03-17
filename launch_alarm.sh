0830575551927cf1dc2108e6702da8aa03af8ab9
rodskin <rod.gilles@gmail.com>
2018-03-17 19:21:02 +0100

    tests

--- launch_alarm.sh
+++ launch_alarm.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 volume=1
 sleep_volume_delay=5
+
+rawurlencode() {
+   local string="${1}"
+   local strlen=${#string}
+   local encoded=""
+
+   for (( pos=0 ; pos<strlen ; pos++ )); do
+      c=${string:$pos:1}
+      case "$c" in
+         [-_.~a-zA-Z0-9] ) o="${c}" ;;
+         * ) printf -v o '%%%02x' "'$c"
+      esac
+     encoded+="${o}"
+   done
+   echo "${encoded}"
+}
+
+# get the time
+LANG='fr'
+TEXT=$( rawurlencode "Il est 9h15" )
+echo $TEXT
+hash="$(echo -n "$TEXT" | md5sum )"
+API="http://translate.google.com/translate_tts?ie=UTF-8&tl=$LANG&q=$TEXT"
+UA="Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.2 Safari/537.36"
+wget -o /dev/null --user-agent="$UA" -O "/tmp/$hash.mp3" "$API"
+#aplay "/tmp/$hash.mp3"
+exit
+
 mpc clear
 mpc random 1
 mpc volume ${volume}
@@ -13,3 +41,14 @@ do
    mpc volume ${volume}
    sleep ${sleep_volume_delay}
 done
+
+# get the time
+TEXT=$( rawurlencode "Il est 9h15" )
+API="http://translate.google.com/translate_tts?ie=UTF-8&tl=$LANG&q=$TEXT"
+
+
+
+
+
+
+
