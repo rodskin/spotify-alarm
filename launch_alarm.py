19fbd795ad1dbed29d152221966f2fed786a068c
pws_francisg <francis.g@pliciweb.com>
2018-03-19 14:28:40 +0100

    code corection, adding config sample file

--- launch_alarm.py
+++ launch_alarm.py
@@ -6,7 +6,8 @@ import os
 import gtts
 import datetime
 import time
-import RPi.GPIO as GPIO 
+import RPi.GPIO as GPIO
+import etc.config
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -18,27 +19,28 @@ def readTime ():
 	tts = gtts.gTTS(text=current_time, lang=lang)
 	tts.save("tmp/time.mp3")
 	os.system('mpg123 tmp/time.mp3')
-	return;
+	return none
 def playPlaylist () :
 	stopPlaylist()
 	os.system('mpc random 1')
 	os.system('mpc volume 100')
 	os.system('mpc add spotify:user:icelandairwaves:playlist:3dNCFy3Q9d6LtGZLWT0c2O')
 	os.system('mpc play')
-	return;
+	return none
 def stopPlaylist () :
 	os.system('mpc clear')
-	return;
+	return none
 
 readTime()
 playPlaylist()
 
-def buttonPress(channel):  
+def buttonPress(channel):
     print "Button pressed!"
     stopPlaylist()
     time.sleep(5)
     readTime()
     playPlaylist()
+    return none
 
 GPIO.add_event_detect(23, GPIO.RISING, callback=buttonPress, bouncetime=300)
 
---
+++ sample/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+volume_music = 100
+lang = 'fr'
+time_snooze = 5
+
+pin_button = 23
+button_bounce_time = 300
